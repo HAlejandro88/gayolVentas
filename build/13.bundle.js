ee86c57bb521c26b4b370f3446b81d64895d7f49
Alejandro <alexsotog82@gmail.com>
2020-12-22 12:06:33 -0600

    remove build directory for git

--- build/13.bundle.js
+++
@@ -1,4 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{81:function(e,n,s){"use strict";s.r(n);var a=s(15);class o extends a.a{render(){return a.c`
-            <h2>Ups!! No encontramos la página que buscabas</h2>
-            <a href="/">Regresar</a>  
-        `}}window.customElements.define("not-found-page",o)}}]);
